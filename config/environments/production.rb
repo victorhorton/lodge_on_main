5ef6ea6b8b2db8efc952672bb6da51ea41ce301e
Victor Horton <vichorton5@gmail.com>
2023-01-12 20:03:27 -0600

    Run devise setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "lodge_on_main_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.default_url_options = { host: 'lodge-on-main.herokuapp.com/' }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
